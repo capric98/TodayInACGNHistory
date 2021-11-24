cea0da2ab4692c0ec8a43fc4313ed1e7c51c23b1
capric98 <42015599+capric98@users.noreply.github.com>
2021-11-24 16:53:26 +0800

    add md2json.py

---
+++ md2json.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+#coding=utf-8
+import argparse
+import os
+import json
+
+import git
+import toml
+
+from datetime import date
+
+def trans(mdf, jsf: os.PathLike, today, commit: str):
+    print(mdf, "->", jsf)
+
+    jobj   = {
+        "events": [],
+        "lastUpdated": {
+            "date": today,
+            "commit": commit
+        }
+    }
+
+    datesuffix = os.path.basename(mdf).split(".")[0]
+    datesuffix = "-{}-{}".format(datesuffix[:2], datesuffix[-2:])
+
+    with open(mdf, encoding="utf-8") as f:
+        event = None
+        for line in f.readlines():
+            # print(line, end="")
+            if line.strip().startswith("# "):
+                if event: jobj["events"].append(event)
+                event  = {
+                    "title": "",
+                    "date": "",
+                    "content": ""
+                }
+                event["title"] = line.split("-", maxsplit=1)[1].strip()
+                event["date"]  = line.split("-")[0].split("# ", maxsplit=1)[1].strip() + datesuffix
+            else:
+                event["content"] = event["content"]+line
+
+    if event: jobj["events"].append(event)
+
+    for k in range(len(jobj["events"])):
+        if jobj["events"][k]["content"].strip().endswith("```"):
+            newcontent = jobj["events"][k]["content"].strip().removesuffix("```")
+            addpos = newcontent.rindex("```toml")
+            # print(newcontent[addpos:].removeprefix("```toml"))
+
+            jobj["events"][k]["content"] = newcontent[:addpos]
+            jobj["addition"] = toml.loads(newcontent[addpos:].removeprefix("```toml"))
+
+        jobj["events"][k]["content"] = jobj["events"][k]["content"].rstrip()
+
+    with open(jsf, mode="w", encoding="utf-8") as f:
+        f.write(json.dumps(jobj))
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Markdown to json.')
+    parser.add_argument("--force", "-F", action=argparse.BooleanOptionalAction, help="force update")
+    parser.add_argument("files", type=str, nargs="*", help="force files")
+
+    args = parser.parse_args()
+
+    today = date.isoformat(date.today())
+
+    repo   = git.Repo()
+    commit = repo.head.object.hexsha
+    flist  = [item.a_path for item in repo.index.diff(None)]
+
+    if args.force:
+        flist = args.files
+        print("Forced to update: {}".format(flist))
+
+    for file in flist:
+        if not(file.endswith(".md") and file.startswith("md")): continue
+
+        fdate  = os.path.basename(file).split(".")[0]
+        lang   = os.path.basename(os.path.dirname(file))
+        target = os.path.join("api/json", fdate+"."+lang+".json")
+
+        trans(file, target, today, commit)
